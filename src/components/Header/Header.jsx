0363a7e5c134e22c224254401ea30f71a983af71
Samir Sadek <samirsadek2009@gmail.com>
2023-09-13 06:56:29 +0600

    Set Layout and make react responsive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,10 @@
-
+import profile from '../../assets/images/profile.png'
 const Header = () => {
     return (
-        <div>
-            <h1 className='text-4xl bg-red-500'>Knowledge Cafe</h1>
-        </div>
+        <header className='flex justify-between items-center p-4 mx-4 border-b-2'>
+            <h1 className='text-4xl font-semibold'>Knowledge Cafe</h1>
+            <img src={profile} alt="" />
+        </header>
     );
 };
 
